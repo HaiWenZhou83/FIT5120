d379f31c74914cb3cc624698dd3dede9e67ef3f9
hzho83 <hzho83@student.monash.edu>
2023-05-06 12:17:36 +0800

    add spinner in add view

--- app/src/main/java/com/example/assignment3/fragment/AddFragment.java
+++ app/src/main/java/com/example/assignment3/fragment/AddFragment.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.assignment3.R;
 import com.example.assignment3.databinding.AddFragmentBinding;
 import com.example.assignment3.viewmodel.SharedViewModel;
 
@@ -21,6 +26,28 @@ public class AddFragment extends Fragment {
         View view = addBinding.getRoot();
         SharedViewModel model = new
                 ViewModelProvider(getActivity()).get(SharedViewModel.class);
+
+//        String[] fitnessItems = {"Aerobic exercise", "Shoulder press", "Bench press"};
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(addBinding.fitnessName.getContext(),
+                R.array.fitnessItems, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        addBinding.spinner.setAdapter(adapter);
+
+        addBinding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object choice = parent.getItemAtPosition(position);
+                //parent.getItemAtPosition(position);
+                Toast.makeText(addBinding.activityMain.getContext(), "your choice is "+choice,
+                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
         addBinding.addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

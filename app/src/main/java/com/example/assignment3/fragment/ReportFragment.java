9cda7d818ace8aec4bb8039d1355734f15f6d4b1
hzho83 <hzho83@student.monash.edu>
2023-05-11 10:19:09 +0800

    change Report screen choose from two types of graphs(bar chart and pie chart done) (function Complete)

--- app/src/main/java/com/example/assignment3/fragment/ReportFragment.java
+++ app/src/main/java/com/example/assignment3/fragment/ReportFragment.java
@@ -12,7 +12,10 @@ import android.view.ViewGroup;
 import android.widget.DatePicker;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.assignment3.R;
 import com.example.assignment3.databinding.ReportFragmentBinding;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.BarData;
@@ -81,39 +84,31 @@ public class ReportFragment extends Fragment {
             }
         });
 
-        List<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(0, 6766));
-        barEntries.add(new BarEntry(1, 4444));
-        barEntries.add(new BarEntry(2, 2222));
-        barEntries.add(new BarEntry(3, 5555));
-        barEntries.add(new BarEntry(4, 1111));
-        barEntries.add(new BarEntry(5, 3656));
-        barEntries.add(new BarEntry(6, 3435));
+        /////////////////////////////////////////
 
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Steps");
-        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        binding.barButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new bar_fragment());
+            }
+        });
 
-        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
-        List<String> xAxisValues = new ArrayList<>(Arrays.asList("Sun", "Mon", "Tues",
-                "Wed", "Thurs", "Fri","Sat"));
-
-        binding.barChart.getXAxis().setValueFormatter(new
-                com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xAxisValues));
-        BarData barData = new BarData(barDataSet);
-        binding.barChart.setData(barData);
-
-        barData.setBarWidth(1.0f);
-        binding.barChart.setVisibility(View.VISIBLE);
-        binding.barChart.animateY(4000);
-//description will be displayed as "Description Label" if not provided
-        Description description = new Description();
-        description.setText("Daily Steps");
-        binding.barChart.setDescription(description);
-//refresh the chart
-        binding.barChart.invalidate();
+        binding.pieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new pie_fragment());
+            }
+        });
 
 
         return view;
+    }
+
+    private void replaceFragment(Fragment nexFragment) {
+        FragmentManager fragmentManager = getChildFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container_view, nexFragment);
+        fragmentTransaction.commit();
     }
 
     private String getTodayDate() {
---
+++ app/src/main/java/com/example/assignment3/fragment/pie_fragment.java
@@ -0,0 +1,59 @@
+package com.example.assignment3.fragment;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import com.example.assignment3.databinding.ChartPieBinding;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pie_fragment extends Fragment {
+
+    public ChartPieBinding binding;
+
+    public pie_fragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = ChartPieBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        List<PieEntry> visitors = new ArrayList<>();
+        visitors.add(new PieEntry(500, "2016"));
+        visitors.add(new PieEntry(600, "2017"));
+        visitors.add(new PieEntry(750, "2018"));
+        visitors.add(new PieEntry(600, "2019"));
+        visitors.add(new PieEntry(670, "2020"));
+
+        PieDataSet pieDataSet = new PieDataSet(visitors, "Visitors");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+
+        PieData pieData = new PieData(pieDataSet);
+
+        binding.pieChart.setData(pieData);
+        binding.pieChart.getDescription().setEnabled(false);
+        binding.pieChart.setCenterText("Visitors");
+        binding.pieChart.animate();
+
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

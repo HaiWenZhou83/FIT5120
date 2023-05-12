67ee5bf10db17ad11fd486803988c7e5bbc3b0df
hzho83 <hzho83@student.monash.edu>
2023-05-12 16:55:45 +0800

    add map

--- app/src/main/java/com/example/assignment3/fragment/MapFragment.java
+++ app/src/main/java/com/example/assignment3/fragment/MapFragment.java
@@ -8,49 +8,72 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.MyLocationStyle;
 import com.example.assignment3.databinding.MapFragmentBinding;
 
 
 public class MapFragment extends Fragment {
-    private MapFragmentBinding addBinding;
+    private MapFragmentBinding binding;
 
     public MapFragment() {}
 
-    //MapView mMapView = null;
+    private MapView mMapView;
+    private AMap aMap = null;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        addBinding = MapFragmentBinding.inflate(inflater, container, false);
-        View view = addBinding.getRoot();
+        binding = MapFragmentBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
-//        SDKInitializer.initialize(getActivity().getApplicationContext());
-//        View view = inflater.inflate(R.layout.map_fragment, container, false);s
-//        mMapView = (MapView) view.findViewById(R.id.bmapView);
+
+        mMapView = binding.map;
+
+        if (aMap == null) {
+            aMap = mMapView.getMap();
+        }
+
+        mMapView.onCreate(savedInstanceState);
+
+        MyLocationStyle myLocationStyle;
+        myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW);
+        myLocationStyle.interval(2000);
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        LatLng latLng = new LatLng(39.906901,116.397972);
+        final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京").snippet("DefaultMarker"));
+
+
         return view;
     }
 
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        addBinding = null;
-//    }
 
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        mMapView.onResume();
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        mMapView.onPause();
-//    }
-//
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        mMapView.onDestroy();
-//    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mMapView.onDestroy();
+    }
+
 }
